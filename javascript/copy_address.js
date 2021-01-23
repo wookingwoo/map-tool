66a3adab9535e2a067632d4863b6308bba6a3d65
JeongHeon Woo <contact@wookingwoo.com>
2021-01-24 01:10:35 +0900

    Add copy_result_msg

--- javascript/copy_address.js
+++ javascript/copy_address.js
@@ -1,39 +1,22 @@
+function copyToClipboard(text) {
 
-  
-  
-  // function copy_address(){
-  //   const successMSG = document.getElementById('success_msg');
-  //   const copy_string = document.querySelector('#copy_string');
-  //   const selection = window.getSelection();
-  //   const range = document.createRange();
-  //   range.selectNode(copy_string);
-  //   selection.removeAllRanges();
-  //   selection.addRange(range);
-    
-  //   try{
-  //     var successful = document.execCommand('copy');
-  //     var resultMSG = successful ? '복사 완료' : '복사 실패'; 
-  //     successMSG.innerHTML=resultMSG;
-  //     selection.removeAllRanges();
-  //   }catch(e){
-  //     successMSG.innerHTML=resultMSG;
-  //   }
-  // }
-  
+  const span_result_MSG = document.getElementById('copy_result_msg');
+
+  var copy_string = document.createElement("textarea");
+
+  document.body.appendChild(copy_string);
+  copy_string.value = text;
+  copy_string.select();
 
 
+  try {
+    var copying = document.execCommand('copy');
+    var resultMSG = copying ? '복사 완료' : '복사 실패';
+    span_result_MSG.innerHTML = resultMSG;
+    document.body.removeChild(copy_string);
+  } catch (e) {
+    span_result_MSG.innerHTML = '복사 실패';
+  }
 
 
-
-  function copyToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-    // cant copy when adding below this code
-    // dummy.style.display = 'none'
-    document.body.appendChild(dummy);
-    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
 }
