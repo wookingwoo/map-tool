fbcafb3b819734592c7b9aa783d57865713741cb
JeongHeon Woo <contact@wookingwoo.com>
2021-01-24 03:02:14 +0900

    Patch error
    {} error

--- javascript/search_coordinate.js
+++ javascript/search_coordinate.js
@@ -39,33 +39,26 @@ function panToCoordinate() {
         // 지도 중심을 부드럽게 이동시킵니다 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
         map.panTo(moveLatLon);
 
-        // // 마커를 생성합니다
-        // var marker = new kakao
-        //     .maps
-        //     .Marker({position: moveLatLon}); //    마커가 지도 위에 표시되도록 설정합니다
-        // marker.setMap(map); //툴팁을 노출하는 마커를 생성합니다.
-
-
+        // 마커를 생성합니다 var marker = new kakao     .maps     .Marker({position:
+        // moveLatLon});     마커가 지도 위에 표시되도록 설정합니다 marker.setMap(map); 툴팁을 노출하는 마커를
+        // 생성합니다.
 
         var iwContent = '<div style="padding:5px;">위도: ' + input_latitude + '</div><div' +
-        ' style="padding:5px;">경도: ' + input_longitude + '</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+                    ' style="padding:5px;">경도: ' + input_longitude + '</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+            iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
-// 인포윈도우를 생성하고 지도에 표시합니다
-var infowindow = new kakao
-.maps
-.InfoWindow({
-    map: map, // 인포윈도우가 표시될 지도
-    position: moveLatLon,
-    content: iwContent,
-    removable: iwRemoveable
-});
-
-}
-
+        // 인포윈도우를 생성하고 지도에 표시합니다
+        var infowindow = new kakao
+            .maps
+            .InfoWindow({
+                map: map, // 인포윈도우가 표시될 지도
+                position: moveLatLon,
+                content: iwContent,
+                removable: iwRemoveable
+            });
 
     } catch (e) {
         alert("입력하신 좌표를 찾을 수 없습니다.\n정확한 좌표를 입력해주세요~\n국내 좌표만 검색이 가능합니다.");
     }
 
-  
+}
