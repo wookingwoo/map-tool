c6d391e000fc13d364ca98246e5f3935e7e36ef8
JeongHeon Woo <contact@wookingwoo.com>
2022-02-02 23:07:23 +0900

    Change popup UI

--- maptool/school/python_project/main.py
+++ maptool/school/python_project/main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import folium
 from folium import Marker
@@ -5,7 +7,8 @@ from folium.plugins import MarkerCluster
 from folium.features import DivIcon
 from tqdm import tqdm
 
-df_school_all = pd.read_csv('./back_data/2021-09-15/전국초중등학교위치표준데이터.csv', encoding='CP949')
+df_school_all = pd.read_csv('./back_data/2021.12.31/1.csv')
+# , encoding='CP949'
 
 seoul_cityhall = [37.5666805, 126.9784147]
 gwangju_cityhall = [35.16015532628239, 126.85146388158552]
@@ -13,7 +16,7 @@ gwangju_cityhall = [35.16015532628239, 126.85146388158552]
 m = folium.Map(location=seoul_cityhall, tiles='openstreetmap', zoom_start=13)
 # m = folium.Map(location=gwangju_cityhall, tiles='openstreetmap', zoom_start=13)
 
-group_all_school = folium.FeatureGroup(name='초중고', overlay=True, show=True)
+group_all_school = folium.FeatureGroup(name='전체 초중고', overlay=True, show=True)
 group_elementary_school = folium.FeatureGroup(name='초등학교', overlay=True, show=False)
 group_middle_school = folium.FeatureGroup(name='중학교', overlay=True, show=False)
 group_high_school = folium.FeatureGroup(name='고등학교', overlay=True, show=False)
@@ -31,21 +34,21 @@ Marker(location=seoul_cityhall,
        icon=folium.Icon(color='red', icon='star')
        ).add_to(m)
 
-df_school_all = df_school_all.dropna(subset=['위도'])
-df_school_all = df_school_all.dropna(subset=['경도'])
+df_school_all = df_school_all.dropna(subset=['Latitude'])
+df_school_all = df_school_all.dropna(subset=['Longitude'])
 
 # 초등학교 DataFrame
-mask_elementary_school = df_school_all['학교급구분'] == '초등학교'
+mask_elementary_school = df_school_all['학교종류명'] == '초등학교'
 df_elementary_school = df_school_all[mask_elementary_school]
 # print(df_elementary_school)
 
 # 중학교 DataFrame
-mask_middle_school = df_school_all['학교급구분'] == '중학교'
+mask_middle_school = df_school_all['학교종류명'] == '중학교'
 df_middle_school = df_school_all[mask_middle_school]
 # print(df_middle_school)
 
 # 고등학교 DataFrame
-mask_high_school = df_school_all['학교급구분'] == '고등학교'
+mask_high_school = df_school_all['학교종류명'] == '고등학교'
 df_high_school = df_school_all[mask_high_school]
 # print(df_high_school)
 
@@ -57,17 +60,19 @@ mc_all_school = MarkerCluster()
 mc_elementary_school = MarkerCluster()
 for _, row in tqdm(df_elementary_school.iterrows()):
     mc_elementary_school.add_child(
-        Marker(location=[row['위도'], row['경도']],
+        Marker(location=[row['Latitude'], row['Longitude']],
                icon=folium.Icon(icon='home', color='blue'),
-               popup=folium.Popup("[초등학교]<br>" + row['학교명'], max_width=300)
-               )
+               popup=folium.Popup(
+                   f"<div style = font-size:1.5em; margin-bottom: 1px;> <strong>[{row['학교명']}]</strong> </div> {row['영문학교명']} <br>표준학교코드: {row['표준학교코드']} <br>도로명주소: {row['도로명주소'] + row['도로명상세주소']} <br>관할조직명: {row['관할조직명']} <br>학교구분: {row['고등학교일반실업구분명']}, {row['설립명']}, {row['남녀공학구분명']}, {row['입시전후기구분명']} <br>전화번호: {row['전화번호']} <br>팩스번호: {row['팩스번호']} <br>홈페이지주소: {row['홈페이지주소']} <br>설립일자/개교기념일: {row['설립일자']}/{row['개교기념일']}<br>정보수정일: {row['수정일']}",
+                   max_width=300), )
     )
 
     mc_all_school.add_child(
-        Marker(location=[row['위도'], row['경도']],
+        Marker(location=[row['Latitude'], row['Longitude']],
                icon=folium.Icon(icon='home', color='blue'),
-               popup=folium.Popup("[초등학교]<br>" + row['학교명'], max_width=300)
-               )
+               popup=folium.Popup(
+                   f"<div style = font-size:1.5em; margin-bottom: 1px;> <strong>[{row['학교명']}]</strong> </div> {row['영문학교명']} <br>표준학교코드: {row['표준학교코드']} <br>도로명주소: {row['도로명주소'] + row['도로명상세주소']} <br>관할조직명: {row['관할조직명']} <br>학교구분: {row['고등학교일반실업구분명']}, {row['설립명']}, {row['남녀공학구분명']}, {row['입시전후기구분명']} <br>전화번호: {row['전화번호']} <br>팩스번호: {row['팩스번호']} <br>홈페이지주소: {row['홈페이지주소']} <br>설립일자/개교기념일: {row['설립일자']}/{row['개교기념일']}<br>정보수정일: {row['수정일']}",
+                   max_width=300), )
     )
 
 group_elementary_school.add_child(mc_elementary_school)
@@ -77,17 +82,19 @@ group_all_school.add_child(mc_all_school)
 mc_middle_school = MarkerCluster()
 for _, row in tqdm(df_middle_school.iterrows()):
     mc_middle_school.add_child(
-        Marker(location=[row['위도'], row['경도']],
+        Marker(location=[row['Latitude'], row['Longitude']],
                icon=folium.Icon(icon='home', color='green'),
-               popup=folium.Popup("[중학교]<br>" + row['학교명'], max_width=300)
-               )
+               popup=folium.Popup(
+                   f"<div style = font-size:1.5em; margin-bottom: 1px;> <strong>[{row['학교명']}]</strong> </div> {row['영문학교명']} <br>표준학교코드: {row['표준학교코드']} <br>도로명주소: {row['도로명주소'] + row['도로명상세주소']} <br>관할조직명: {row['관할조직명']} <br>학교구분: {row['고등학교일반실업구분명']}, {row['설립명']}, {row['남녀공학구분명']}, {row['입시전후기구분명']} <br>전화번호: {row['전화번호']} <br>팩스번호: {row['팩스번호']} <br>홈페이지주소: {row['홈페이지주소']} <br>설립일자/개교기념일: {row['설립일자']}/{row['개교기념일']}<br>정보수정일: {row['수정일']}",
+                   max_width=300), )
     )
 
     mc_all_school.add_child(
-        Marker(location=[row['위도'], row['경도']],
+        Marker(location=[row['Latitude'], row['Longitude']],
                icon=folium.Icon(icon='home', color='green'),
-               popup=folium.Popup("[중학교]<br>" + row['학교명'], max_width=300)
-               )
+               popup=folium.Popup(
+                   f"<div style = font-size:1.5em; margin-bottom: 1px;> <strong>[{row['학교명']}]</strong> </div> {row['영문학교명']} <br>표준학교코드: {row['표준학교코드']} <br>도로명주소: {row['도로명주소'] + row['도로명상세주소']} <br>관할조직명: {row['관할조직명']} <br>학교구분: {row['고등학교일반실업구분명']}, {row['설립명']}, {row['남녀공학구분명']}, {row['입시전후기구분명']} <br>전화번호: {row['전화번호']} <br>팩스번호: {row['팩스번호']} <br>홈페이지주소: {row['홈페이지주소']} <br>설립일자/개교기념일: {row['설립일자']}/{row['개교기념일']}<br>정보수정일: {row['수정일']}",
+                   max_width=300), )
     )
 
 group_middle_school.add_child(mc_middle_school)
@@ -97,23 +104,26 @@ group_all_school.add_child(mc_all_school)
 mc_high_school = MarkerCluster()
 for _, row in tqdm(df_high_school.iterrows()):
     mc_high_school.add_child(
-        Marker(location=[row['위도'], row['경도']],
+        Marker(location=[row['Latitude'], row['Longitude']],
                icon=folium.Icon(icon='home', color='orange'),
-               popup=folium.Popup("[고등학교]<br>" + row['학교명'], max_width=300)
+               popup=folium.Popup(
+                   f"<div style = font-size:1.5em; margin-bottom: 1px;> <strong>[{row['학교명']}]</strong> </div> {row['영문학교명']} <br>표준학교코드: {row['표준학교코드']} <br>도로명주소: {row['도로명주소'] + row['도로명상세주소']} <br>관할조직명: {row['관할조직명']} <br>학교구분: {row['고등학교일반실업구분명']}, {row['설립명']}, {row['남녀공학구분명']}, {row['입시전후기구분명']} <br>전화번호: {row['전화번호']} <br>팩스번호: {row['팩스번호']} <br>홈페이지주소: {row['홈페이지주소']} <br>설립일자/개교기념일: {row['설립일자']}/{row['개교기념일']}<br>정보수정일: {row['수정일']}",
+                   max_width=300),
                )
     )
 
     mc_all_school.add_child(
-        Marker(location=[row['위도'], row['경도']],
+        Marker(location=[row['Latitude'], row['Longitude']],
                icon=folium.Icon(icon='home', color='orange'),
-               popup=folium.Popup("[고등학교]<br>" + row['학교명'], max_width=300)
-               )
+               popup=folium.Popup(
+                   f"<div style = font-size:1.5em; margin-bottom: 1px;> <strong>[{row['학교명']}]</strong> </div> {row['영문학교명']} <br>표준학교코드: {row['표준학교코드']} <br>도로명주소: {row['도로명주소'] + row['도로명상세주소']} <br>관할조직명: {row['관할조직명']} <br>학교구분: {row['고등학교일반실업구분명']}, {row['설립명']}, {row['남녀공학구분명']}, {row['입시전후기구분명']} <br>전화번호: {row['전화번호']} <br>팩스번호: {row['팩스번호']} <br>홈페이지주소: {row['홈페이지주소']} <br>설립일자/개교기념일: {row['설립일자']}/{row['개교기념일']}<br>정보수정일: {row['수정일']}",
+                   max_width=300), )
     )
 
 group_high_school.add_child(mc_high_school)
 group_all_school.add_child(mc_all_school)
 
-title1 = '전국 초중고교 위치'
+title1 = '[우킹우 맵툴] 전국 초중고교 지도'
 title_html1 = '''
              <h3 align="center" ><b>{}</b></h3>
              '''.format(title1)
